705ca0de2f4cacee95d14c85e8531a5334171bd2
reshamgupta13 <reshamgupta2005@gmail.com>
2025-11-09 11:07:11 +0000

    Rotate an array by one

---
+++ Rotate_arr_by_one.java
@@ -0,0 +1,9 @@
+class Solution {
+    public void rotate(int[] arr) {
+        int temp=arr[arr.length-1];
+        for(int i=arr.length-2; i>=0; i--){
+            arr[i+1]=arr[i];
+        }
+             arr[0]=temp;
+    }
+}
