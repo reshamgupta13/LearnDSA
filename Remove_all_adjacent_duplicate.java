214b694752cf6b0ecc7dfaa5bdfa75f7a5750fa8
reshamgupta13 <reshamgupta2005@gmail.com>
2025-11-15 05:13:11 +0000

    Remove all adjacent duplicate in String

---
+++ Remove_all_adjacent_duplicate.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String removeDuplicates(String s) {
+        int n=s.length();
+        char[] result=new char[n];
+
+        int j=0;
+        for(int i=0;i<n;i++){
+            char ch=s.charAt(i);
+            if(j>0 && ch==result[j-1]){
+                j--;
+                result[j]='\0';
+            }
+            else{
+                result[j++]=ch;
+            }
+        }
+        return new String(result , 0 , j);
+    }
+}
