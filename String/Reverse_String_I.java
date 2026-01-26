3553d6b34ace1758b6470bcdfd8be5fcb598bd64
Resham Gupta <reshamgupta2005@gmail.com>
2026-01-26 14:24:05 +0000

    Reverse_String_I

---
+++ String/Reverse_String_I.java
@@ -0,0 +1,19 @@
+/*Example 1:
+
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+*/
+
+/*Solution*/
+class Solution {
+    public void reverseString(char[] s) {
+      int left=0, right=s.length-1;
+      while(left<right){
+        char temp=s[left];
+        s[left]=s[right];
+        s[right]=temp;
+        left++;
+        right--;
+      }     
+    }
+}
