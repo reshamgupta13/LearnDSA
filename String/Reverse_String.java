fcc584881e2e2dcfd739c56ee53cd49062bfbbb6
Resham Gupta <reshamgupta2005@gmail.com>
2026-01-26 14:20:19 +0000

    Reverse_String II

--- String/Reverse_String.java
+++ String/Reverse_String.java
@@ -1,1 +1,29 @@
+/*Example 1:
+
+Input: s = "abcdefg", k = 2
+Output: "bacdfeg"*/
+
+/*Solution*/
+class Solution {
+    public void reverse(char[] arr, int i,int j){
+        while(i<=j){
+        char t=arr[i];
+        arr[i]=arr[j];
+        arr[j]=t;
+        i++;
+        j--;
+        }
+    }
+    public String reverseStr(String s, int k) {
+        char[] arr=s.toCharArray();
+        int n=s.length();
+        int i=0;
+        while(i<n){
+            int j=Math.min(i+k-1, n-1);
+            reverse(arr,i,j);
+            i+=2*k;
+    }
+    return new String(arr);
+}
+}
 
