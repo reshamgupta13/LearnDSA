0ce2167b2278e1a0d1140af6ed01b3c34b791fbf
Resham Gupta <reshamgupta2005@gmail.com>
2026-01-28 18:35:09 +0000

    Valid_Anagram

--- String/Valid_Anagram.java
+++ String/Valid_Anagram.java
@@ -1,1 +1,22 @@
+/*Example 1:
 
+Input: s = "anagram", t = "nagaram"
+
+Output: true*/
+
+/*Solution */
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        String s1 = s.toLowerCase().replaceAll("\\s", "");
+        String s2 = t.toLowerCase().replaceAll("\\s", "");
+         boolean isAnagram = false;
+        if (s1.length() == s2.length()) {
+            char[] arr1 = s1.toCharArray();
+            char[] arr2 = s2.toCharArray();
+            Arrays.sort(arr1);
+            Arrays.sort(arr2);
+            isAnagram = Arrays.equals(arr1, arr2);
+        }
+        return isAnagram;
+    }
+}
