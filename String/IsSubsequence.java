81fa3a0cd618ebe3b13bdff65e41dfc5ddd5e40d
Resham Gupta <reshamgupta2005@gmail.com>
2026-01-30 18:39:23 +0000

    IsSubsequence

--- String/IsSubsequence.java
+++ String/IsSubsequence.java
@@ -1,1 +1,31 @@
+/* Example 1:
 
+Input: s = "abc", t = "ahbgdc"
+Output: true
+Example 2:
+
+Input: s = "axc", t = "ahbgdc"
+Output: false */
+
+
+/* Solution*/
+
+class Solution{
+    public boolean isSubsequence(String s, String t){
+        int i=0, j=0;
+        while(i<s.length() && j<t.length()){
+            if(s.charAt(i)==t.charAt(j)){
+                i++;
+                j++;
+            }
+            else{
+                j++;
+            }
+        }
+        if(i==s.length()){
+            return true;
+    
+        }
+                return false;
+    }
+}
