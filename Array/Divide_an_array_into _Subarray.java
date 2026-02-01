873ae894c3425c0193a3a34ba26de26baa93c667
Resham Gupta <reshamgupta2005@gmail.com>
2026-02-01 08:26:54 +0000

    Divide_an_array_into_subaaray

--- Array/Divide_an_array_into _Subarray.java
+++ Array/Divide_an_array_into _Subarray.java
@@ -1,1 +1,39 @@
+/*You are given an array of integers nums of length n.
 
+The cost of an array is the value of its first element. For example, the cost of [1,2,3] is 1 while the cost of [3,4,1] is 3.
+
+You need to divide nums into 3 disjoint contiguous subarrays.
+
+Return the minimum possible sum of the cost of these subarrays.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,12]
+Output: 6
+Explanation: The best possible way to form 3 subarrays is: [1], [2], and [3,12] at a total cost of 1 + 2 + 3 = 6.
+The other possible ways to form 3 subarrays are:
+- [1], [2,3], and [12] at a total cost of 1 + 2 + 12 = 15.
+- [1,2], [3], and [12] at a total cost of 1 + 3 + 12 = 16. */
+
+/*Solution */
+
+class Solution {
+    public int minimumCost(int[] nums) {
+         int first = nums[0];
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < min1) {
+                min2 = min1;
+                min1 = nums[i];
+            } else if (nums[i] < min2) {
+                min2 = nums[i];
+            }
+        }
+
+        return first + min1 + min2;
+    }
+}
